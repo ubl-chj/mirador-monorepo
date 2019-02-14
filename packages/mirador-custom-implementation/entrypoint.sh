7878418fa088d20a206fc097d29afb8a35460175
Christopher Johnson <chjohnson39@gmail.com>
2019-02-14 17:58:01 +0100

    adds docker-compose integration testing

--- packages/mirador-custom-implementation/entrypoint.sh
+++ packages/mirador-custom-implementation/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /srv/mirador/packages/client
+cd /srv/mirador/packages/mirador-custom-implementation
 exec serve "$@"
---
+++ packages/mirador-e2e-tests/cypress/integration/basic.spec.js
@@ -0,0 +1,14 @@
+/* global cy */
+describe('Mirador Base', () => {
+  it('Visits Mirador Base', () => {
+    cy.visit('http://localhost:5000/view')
+    cy.get('title').contains('Mirador')
+    cy.get('#addBtn').click()
+    cy.get('#manifestURL').type('https://purl.stanford.edu/sn904cj3429/iiif/manifest')
+    cy.get('#fetchBtn').click()
+    cy.get('li').contains('https://purl.stanford.edu/sn904cj3429/iiif/manifest')
+    cy.get('h3').contains('Peter\'s San Francisco Locator. The Birds-Eye-View Map of the Exposition City. Published by Locator Publishing Co')
+    cy.get('li>button').click()
+    cy.get('.mirador-window>img').should('have.attr', 'src', 'https://stacks.stanford.edu/image/iiif/sn904cj3429%2F12027000/full/!400,400/0/default.jpg')
+  })
+})
